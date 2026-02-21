aee1a8cb83e0c47df9d4d3f7fff2255837602143
Suleiman Yusuf kaita <Suleiman20015kaita@gmail.com>
2026-02-21 20:47:44 +0100

    sas

--- src/Layout/_layout.jsx
+++ src/Layout/_layout.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import Header from './Header';
-import Sidebar from './Sidebar';
+import Sidebar from './SideBar';
 import Fotter from './fotter'; 
 import { Outlet } from 'react-router-dom';
 
